bbd13ea3027678dc3093b053d6b56f98e003857c
BleedScreamShort <BleedScream@gmail.com>
2025-06-15 19:22:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,4 @@ def index():
 
 # ===================== Webhook init =========================
 if __name__ == "__main__":
-    webhook_url = "https://print-storage-bot.onrender.com/" + API_TOKEN
-    bot.remove_webhook()
-    bot.set_webhook(url=webhook_url)
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    bot.polling(none_stop=True)
